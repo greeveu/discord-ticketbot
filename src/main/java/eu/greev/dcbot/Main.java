7e61bef368594e93bfb10662cffdf01fc751ade6
Jannik <jannikjentsch2006@gmail.com>
2022-08-11 15:33:11 +0200

    Extended base structure and added /ping command

--- src/main/java/eu/greev/dcbot/Main.java
+++ src/main/java/eu/greev/dcbot/Main.java
@@ -1,48 +1,78 @@
 package eu.greev.dcbot;
 
+import eu.greev.dcbot.utils.Data;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.PropertyConfigurator;
 import org.jetbrains.annotations.NotNull;
 
 import javax.security.auth.login.LoginException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Main extends ListenerAdapter {
+
     public static void main(String[] args) throws InterruptedException, LoginException {
-        JDA jda = JDABuilder.createLight("", GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES, GatewayIntent.GUILD_EMOJIS_AND_STICKERS, GatewayIntent.GUILD_MEMBERS)
-                .addEventListeners(new Main())
-                .setActivity(Activity.listening(" to ticket commands."))
+        String log4jConfPath = "./src/main/resources/log4j.properties";
+        PropertyConfigurator.configure(log4jConfPath);
+
+        JDA jda;
+        jda = JDABuilder.createLight(new Data().botToken, GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES, GatewayIntent.GUILD_EMOJIS_AND_STICKERS, GatewayIntent.GUILD_MEMBERS)
+                .setActivity(Activity.listening(" ticket commands."))
                 .setStatus(OnlineStatus.ONLINE)
                 .build();
+        jda.addEventListener(new Main());
         jda.awaitReady();
 
         List<CommandData> commands = new ArrayList<>();
         createCommands(commands);
-        jda.getGuildById(301471248393830413L).updateCommands().addCommands(commands).queue();
+        jda.upsertCommand(Commands.slash("ping", "Returns the ping of the bot")).queue();
+        jda.getGuildById(new Data().testID).updateCommands().addCommands(commands).queue();
+
+        String day = new SimpleDateFormat("dd-MM-yy hh:mm aa ").format(new Date(System.currentTimeMillis()));
+        LogManager.getLogger(Main.class).log(Level.INFO, "Started: " + day);
     }
 
-    private static void createCommands(@NotNull List<CommandData> commands) {
-        //Temporary
+    private static void createCommands(List<CommandData> commands) {
         commands.add(Commands.slash("ticket", "Manage the ticket system")
                 .addSubcommands(new SubcommandData("setup", "Setup the System"))
                 .addSubcommands(new SubcommandData("add", "Add a User to this ticket")
                         .addOption(OptionType.USER,"member", "The user adding to the current ticket", true))
                 .addSubcommands(new SubcommandData("remove", "Remove a User from this ticket")
                         .addOption(OptionType.USER,"member", "The user removing from the current ticket", true))
-                .addSubcommands(new SubcommandData("create", "Create a new Ticket for you"))
+                .addSubcommands(new SubcommandData("create", "Create a new Ticket for you")
+                        .addOption(OptionType.STRING, "topic", "The topic of the ticket", false))
                 .addSubcommands(new SubcommandData("close", "Close this ticket")
                         .addOption(OptionType.STRING, "reason", "The reason the ticket was closed", false))
                 .addSubcommands(new SubcommandData("claim", "Claim this ticket"))
-                .addSubcommands(new SubcommandData("clear-config", "Clear the config for the TicketManager"))
+                .addSubcommands(new SubcommandData("help", "Display help menu"))
+                .addSubcommands(new SubcommandData("set-handler", "Sets the new handler"))
+
+                //.addSubcommands(new SubcommandData("clear-config", "Clear the config for the TicketManager"))
         );
+    }
+
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        if (event.getName().equals("ping")) {
+            long time = System.currentTimeMillis();
+            event.reply("Pong!").setEphemeral(true)
+                    .flatMap(v ->
+                            event.getHook().editOriginalFormat("Pong: %d ms", System.currentTimeMillis() - time)
+                    ).queue();
+        }
     }
 }
